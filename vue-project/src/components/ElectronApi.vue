1848ed4c62ca115de9c9b03c2abbebb24a920999
magnus <magnus@connoisseur.se>
2022-06-01 09:37:26 +0200

    WIP

---
+++ vue-project/src/components/ElectronApi.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Number of CPUs: {{ cpuCount }}
+  </div>
+</template>
+<script>
+export default {
+  name: "ElectronApi",
+  computed: {
+    cpuCount() {
+      return window.api.cpuCount()
+    },
+  },
+}
+</script>
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+import ElectronApi from "../components/ElectronApi.vue";
+</script>
+
+<template>
+  <main>
+    hej
+    <ElectronApi></ElectronApi>
+  </main>
+</template>
